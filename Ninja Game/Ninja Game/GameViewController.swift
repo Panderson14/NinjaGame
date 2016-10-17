f365f182048270653b322f3feeeabd0e33e1952b
Patrick Anderson <psa5dg@virginia.edu>
2016-10-16 22:31:35 -0400

    Ninja Game

---
+++ Ninja Game/Ninja Game/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  Ninja Game
+//
+//  Created by Patrick Anderson on 10/16/16.
+//  Copyright © 2016 Patrick Anderson. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+}
